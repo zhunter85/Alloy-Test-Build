89be8aa5d72d4a32a2caa8423a5194bc2563eb43
zhunter85 <107071468+zhunter85@users.noreply.github.com>
2026-09-16 16:25:46 -0700

    Update vite.config.js
    add host a-ta-01m2p82g9nx19k1fr7mrmdkp2s-z1fsyjsfph4lb8k8qot7kwa4s.w.modal.host

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "0.0.0.0",
+    host: "a-ta-01m2p82g9nx19k1fr7mrmdkp2s-z1fsyjsfph4lb8k8qot7kwa4s.w.modal.host",
     port: 3000,
     strictPort: true,
   },
